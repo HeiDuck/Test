378a53fe30eebbe86aff700000b683778929e4b5
li.shuhao <li.shuhao@lifecycle.cn>
2017-03-23 15:38:57 +0800

    no message

---
+++ test/TestHttp.java
@@ -0,0 +1,53 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+public class TestHttp {
+	public String result;
+	public static void main(String[] args) throws Exception, Exception {
+		
+	}
+	TestHttp(){}
+	public void getMessage() throws Exception{
+		String url = "http://192.168.100.18:10120/servlets/binserv/B2B"; 
+		String command = "com.agilecontrol.phone.B2BCmd";
+		String cmd = "b2b.login";
+		String username = "root";
+		String password = "lifecycle";
+		HttpClient client = HttpClients.createDefault();
+		HttpPost request = new HttpPost(url);
+		List <NameValuePair> nvp = new ArrayList<NameValuePair>();
+		JSONObject transaction = new JSONObject();
+		JSONObject params = new JSONObject();
+		
+		params.put("cmd",  cmd);
+		params.put("username",  username);
+		params.put("password",  password);
+		transaction.put("params", params);
+		transaction.put("command",  command);
+		
+		nvp.add(new BasicNameValuePair("transaction", transaction.toString()));
+		request.setEntity(new UrlEncodedFormEntity(nvp,HTTP.UTF_8)); 
+		
+		System.out.println("Executing request " + request.getRequestLine());
+		
+		HttpResponse response = client.execute(request);
+		HttpEntity httpEntity = response.getEntity();  
+        String result = EntityUtils.toString(httpEntity);//È¡³öÓ¦´ð×Ö·û´®  
+		System.out.println("Response Code: " + result);
+		this.result = result;
+	}
+}
